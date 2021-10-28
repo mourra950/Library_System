74eb50255e810928d2ccf74571d3e19caddf6518
DELL <mohamada14@gmail.com>
2021-10-29 01:55:55 +0200

    GUI Payment

---
+++ src/Controller/VisaOrCashPaymentController.java
@@ -0,0 +1,29 @@
+package Controller;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class VisaOrCashPaymentController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void VisaPayment(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("VisaPayment.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void CashPayment(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("CashPayment.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
