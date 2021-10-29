59a640489e94368453213e72c73b971144afd7ff
DELL <mohamada14@gmail.com>
2021-10-29 15:47:47 +0200

    Late payment Adjusment 2

--- src/Classes/LatePayment.java
+++ src/Classes/LatePayment.java
@@ -11,27 +11,15 @@ public class LatePayment extends Payment {
 //        super();
 //        this.daysLate = daysLate;
 //    }
-
-    public int getDaysLate() {
-        return daysLate;
-    }
-
-    public void setDaysLate(int daysLate) {
-        this.daysLate = daysLate;
-    }
-
     public double getLatefees() {
         return latefees;
     }
 
-    public void setLatefees(double latefees) {
-        this.latefees = latefees;
-    }
-    public double getdaysLate(User a) {
+    public void setLatefees(User a) {
         if (a.IsDeadLine() == true) {
-discount=0;
+            discount = 0;
+            this.latefees = latefees;
         }
-        return discount;
     }
 }
 
