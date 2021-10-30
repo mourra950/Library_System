9bc73d458e536bc8e6601bcde1820c651f8986a8
Fathy24 <fathyy249@gmail.com>
2021-10-30 17:28:08 +0200

    Fathy's First Commit

---
+++ src/Classes/Person.java
@@ -0,0 +1,19 @@
+package Classes;
+
+public class Person {
+    protected String name;
+    protected String address;
+    protected String number;
+
+    public Person(String name, String address, String number){
+
+        this.name=name;
+        this.address=address;
+        this.number=number;
+
+    }
+public Person(){
+
+}
+
+}
