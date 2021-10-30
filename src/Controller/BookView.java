560161e71b741699e7d8fb186ed970b922bedbdf
Fathy24 <fathyy249@gmail.com>
2021-10-30 17:47:57 +0200

    Fathy's First Commit

---
+++ src/Controller/BookView.java
@@ -0,0 +1,49 @@
+package Controller;
+
+import Classes.LibraryCollection;
+import Classes.book;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookView implements Initializable {
+
+    @FXML
+    private TableColumn<LibraryCollection, String> Author;
+
+    @FXML
+    private TableColumn<LibraryCollection, String> Genre;
+
+    @FXML
+    private TableColumn<LibraryCollection, Integer> Id;
+
+    @FXML
+    private TableColumn<LibraryCollection, String> Title;
+
+    @FXML
+    private TableColumn<LibraryCollection, String> Topic;
+
+    @FXML
+    private TableView<book> table;
+
+    ObservableList<book> list= FXCollections.observableArrayList(
+            new book("zeby", 123,"omar","zeby","rrrrr"),
+            new book("zeby", 12,"moniem","zebomar","zeb")
+    );
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Author.setCellValueFactory(new PropertyValueFactory<LibraryCollection,String>("Author"));
+        Genre.setCellValueFactory(new PropertyValueFactory<LibraryCollection,String>("Genre"));
+        Id.setCellValueFactory(new PropertyValueFactory<LibraryCollection,Integer>("Id"));
+        Title.setCellValueFactory(new PropertyValueFactory<LibraryCollection,String>("Title"));
+        Topic.setCellValueFactory(new PropertyValueFactory<LibraryCollection,String>("Topic"));
+        table.setItems(list);
+    }
+}
