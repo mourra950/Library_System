d5fae35c651ee0a681e036a4f09c019e9b56b6f7
Kareem Azab <kaazab123@hotmail.com>
2021-10-30 17:56:08 +0200

    Modify User and Librarian Classes

--- src/Classes/User.java
+++ src/Classes/User.java
@@ -4,18 +4,21 @@ import java.time.LocalDate;
 
 public class User extends Person{
 
+    private String person_id;
 
     private int BooksBorrowed;
 
 
-    public User(){
+    public User(String k){
        super();
+    person_id = k;
 
-
-        connect.testjdbc.connect("INSERT INTO `main`.`Users`(`Mail`,`Name`,`Password`) VALUES ('"+super.name+"','"+super.address+"','"+super.number+"');");
+        connect.testjdbc.connect("INSERT INTO `main`.`Users`(`Mail`,`Name`,`Password`) VALUES ('"+super.name+"','"+super.Email+"','"+super.Password+"');");
+        connect.testjdbc.connect("INSERT INTO `main`.`Users`(`person_id`) VALUES ('"+person_id+"');");
 
     }
     public String getUserName(){return super.name;}
+    public String getPersonId(){return person_id;}
 
 
 
