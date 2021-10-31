7475c1c5dd34560497921767db7fe4677c87ed48
DELL <mohamada14@gmail.com>
2021-10-31 17:33:54 +0200

    payment update.

--- src/Classes/Payment.java
+++ src/Classes/Payment.java
@@ -1,5 +1,6 @@
 package Classes;
-
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDate;
 public class Payment {
     double price;
     int quantity;
@@ -12,29 +13,35 @@ public class Payment {
     public double discountvalue(int quantity){
         if (quantity>1 && quantity<=3 )
         {
-            discount=(price*quantity)*0.25;
+            return (price*quantity)*0.25;
         }
         if (quantity>3 && quantity<=5 )
         {
-            discount=(price*quantity)*0.5;
+            return (price*quantity)*0.5;
         }
         return 0;
     }
-    public void setLatefees(book a) {
-        if (a.IsDeadLine()) {
-            latesfees=getfprice()*0.1;
-        }
-    }
-    public double getLatefees() {
-        return latesfees;
-    }
     public double getfprice(){
-        return ((price*quantity)-discount);
+        return ((price*quantity)-discountvalue(quantity));
     }
     public void display(){
         System.out.println("your number of borrowed books "+ quantity);
         System.out.println("your discount is "+discountvalue(quantity));
-        System.out.println("the total price is "+(getfprice()+getLatefees()));
+        //System.out.println("the total price is "+(getfprice()+//getLatefees()));
     }
-
+    public double getLatefees(book b){
+        long daysBetween=ChronoUnit.DAYS.between(b.getEndDate(), LocalDate.now());
+        if (daysBetween<0){
+            return 0;
+}
+        if (daysBetween>=1&&daysBetween<7){
+            return getfprice()*0.1;
+        }
+        if (daysBetween>=7&&daysBetween<14){
+            return getfprice()*0.2;
+        }
+else{
+            return getfprice()*0.5;
+        }
+    }
 }
