d4fb2c8ddd747642218507be2b3121624f20476b
Kareem Azab <kaazab123@hotmail.com>
2021-10-31 19:33:10 +0200

    nanananan

--- src/Classes/Librarian.java
+++ src/Classes/Librarian.java
@@ -16,7 +16,7 @@ public class Librarian extends Person{
 
     public void addbook(book b1,Library s){
         connect.testjdbc.connect("INSERT INTO `main`.`Books`(`Title`,`Id`,`Author`,`Genre`,`Count`,`BorrowCount`) VALUES ('"+b1.getTitle()+",'"+b1.getId()+",'"+b1.getAuthor()+",'"+b1.getGenre()+",'"+b1.getCount()+"',0);");
-        connect.testjdbc.connect("INSERT INTO `main`.`library`(`id`,`name``book_id`) VALUES ('"+b1.getTitle()+",'"+b1.getId()+",'"+b1.getAuthor()+",'"+b1.getGenre()+",'"+b1.getCount()+"',0);");
+        connect.testjdbc.connect("INSERT INTO `main`.`library`(`id`,`name``book_id`) VALUES ('"+s.getId()+",'"+s.getName()+",'"+b1.getId()+");");
     }
     public void removebook(book b1){
         connect.testjdbc.connect("DELETE FROM `main`.`Books` WHERE (`Id`='"+b1.getId()+"');");
