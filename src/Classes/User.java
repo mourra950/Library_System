2314e5ea0b2f99c1df242e7456636d984af9d21c
Kareem Azab <kaazab123@hotmail.com>
2021-10-31 15:27:43 +0200

    Join Tables together in the database

--- src/Classes/User.java
+++ src/Classes/User.java
@@ -5,13 +5,13 @@ import java.time.LocalDate;
 public class User extends Person{
 
     private String person_id;
+
     private int BooksBorrowed;
 
 
     public User(String k){
-        super();
-        person_id = k;
-        int BooksBorrowed=0;
+       super();
+    person_id = k;
 
         connect.testjdbc.connect("INSERT INTO `main`.`person`(`Mail`,`Name`,`Password`) VALUES ('"+super.name+"','"+super.Email+"','"+super.Password+"');");
         connect.testjdbc.connect("INSERT INTO `main`.`Users`(`person_id`) VALUES ('"+person_id+"');");
@@ -20,12 +20,7 @@ public class User extends Person{
     public String getUserName(){return super.name;}
     public String getPersonId(){return person_id;}
 
-    public void setBooksBorrowed(int booksBorrowed) {
-        BooksBorrowed = booksBorrowed;
-    }
-    public int getBooksBorrowed(){
-        return BooksBorrowed;
-    }
+
 
     public boolean BorrowedValid(){
         return BooksBorrowed <= 5;
@@ -34,26 +29,17 @@ public class User extends Person{
 
 
     public void borrow(book b,int duration){
-        if (this.getBooksBorrowed()<5){
-            if (b.getCan_be_checked_out() && b.getIs_available()){
-                b.setIs_available(false);
-                b.setCount(b.getCount()+1);
-                this.setBooksBorrowed(this.getBooksBorrowed()+1);
-                b.setStartDate(LocalDate.now());
-                b.setEndDate(b.getStartDate().plusDays(duration));
-                System.out.println("you should return before "+b.getEndDate()+" days");
-                connect.testjdbc.connect("");
+        if (b.getCan_be_checked_out() && b.getIs_available()){
+            b.setIs_available(false);
+            b.setStartDate(LocalDate.now());
+            b.setEndDate(b.getStartDate().plusDays(duration));
+            System.out.println("you should return before "+b.getEndDate()+" days");
 
-            }
-             else{
-                    System.out.println("Book cannot be borrowed");
-                }
-         }
+        }
         else{
-            System.out.println("limit reached!");
-            }
+            System.out.println("Book cannot be borrowed");
         }
     }
-
+    }
 
 
