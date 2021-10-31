812a80807d69b4713defa53d3cb19551ec26afe9
Fathy24 <fathyy249@gmail.com>
2021-10-31 19:30:38 +0200

    Library class update

--- src/Classes/Library.java
+++ src/Classes/Library.java
@@ -3,17 +3,18 @@ package Classes;
 public class Library {
     private String id;
     private String name; //branch name
-    private String book_id[];
-    static private int BooksLimit =50;
-    public Library(String id, String name, book books[]) {
+    public Library(String id, String name) {
         this.id = id;
         this.name = name;
-        for(int i=0;i<BooksLimit;i++){
-            book_id[i] = books[i].getId();
-        }
     }
     public Library(String id){
-        this.id=id;
+        this.id = id;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
     }
 }
 
