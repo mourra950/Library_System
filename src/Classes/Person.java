47c50fab63e48588f0a3afae919f7da74c562f95
ZiadAssem <ziad.assem2001@gmail.com>
2021-11-01 01:33:56 +0200

    Added search

--- src/Classes/Person.java
+++ src/Classes/Person.java
@@ -1,19 +1,53 @@
 package Classes;
 
+import connect.testjdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Person {
     protected String name;
     protected String Email;
     protected String Password;
 
 
-    public Person(String name, String Email, String Password){
-        this.name=name;
-        this.Email=Email;
-        this.Password=Password;
+    public Person(String name, String Email, String Password) {
+        this.name = name;
+        this.Email = Email;
+        this.Password = Password;
 
     }
-public Person(){
 
+    public Person() {
+
+    }
+
+    public void searchId(String id) {
+        Connection con = testjdbc.connect();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            String sql = "SELECT Title FROM `main`.`Books` where Id=?";
+            ps = con.prepareStatement(sql);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            id = rs.getString(1);
+            System.out.println(id);
+            //  connect.testjdbc.connect("SELECT FROM `main`.`Books` WHERE (`Id`='"+id+"');");
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
 }
 
-}
