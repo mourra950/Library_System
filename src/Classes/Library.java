ab72bf30ae6b8b6d03f7ee13c87496d203dfc762
Kareem Azab <kaazab123@hotmail.com>
2021-10-31 16:44:29 +0200

    add library class

---
+++ src/Classes/Library.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class Library {
+    private String id;
+    private String name;
+
+
+    public Library(String id, String name, book b1) {
+        this.id = id;
+        this.name = name;
+
+    }
+    public Library(String id){
+        this.id=id;
+    }
+}
+
